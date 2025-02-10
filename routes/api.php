2988870e4cf063ff2a5b99159784570250825159
elijah Sunwa <elijahnyaroya@gmail.com>
2025-02-10 11:38:11 +0300

    10 February 2025
    Time : 1137 hrs
    
    remove unavailable routes from api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\curlTestController;
-use App\Http\Controllers\mpesaPaymentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,20 +17,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Get list of meetings.
-Route::get('/meetings', 'Zoom\MeetingController@list');
-
-// Create meeting room using topic, agenda, start_time.
-//Route::post('/updateMeeting', [\App\Http\Controllers\zoomController::class,"updateMeeting"]);
-Route::post('/updateMeeting', [\App\Http\Controllers\zoomController::class,"update"]);
-Route::post('/meetings', [\App\Http\Controllers\zoomController::class,"creaeZoomMeetingx"]);
-//Route::post('/meetings', [\App\Http\Controllers\zoomController::class,"create"]);
-
-// Get information of the meeting room by ID.
-Route::get('/meetings/{id}', 'Zoom\MeetingController@get')->where('id', '[0-9]+');
-Route::patch('/meetings/{id}', 'Zoom\MeetingController@update')->where('id', '[0-9]+');
-Route::delete('/meetings/{id}', 'Zoom\MeetingController@delete')->where('id', '[0-9]+');
-
-Route::post('/testcurl', [curlTestController::class,"testCurlCalls"]);
-Route::post('/mpesaresponse', [mpesaPaymentController::class,"mpesaCallback"]);
-//Route::post('/callback', [mpesaPaymentController::class,"mpesaCallbackFromAnotherServer"]);
